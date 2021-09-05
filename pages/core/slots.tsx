86983f888ce8fdfdb3d11b5109494dca712372a7
Pol Moneys <pmones@wideagency.com>
2021-09-05 20:04:35 +0200

    💐 Improve demos

--- pages/core/slots.tsx
+++ pages/core/slots.tsx
@@ -1,35 +1,115 @@
 import { HelveticaNeue, Page, Title, SpacerParagraph, SpacerSection } from '@/composed';
 import { Card, Grid } from '@/core';
 import SlotCard from '@/features/slots/Card';
-import SlotsDemo from '@/features/slots/Intro';
-import Stat from '@/features/slots/Stat';
+import Code from '@/features/tutorial/Code';
 import styles from '@/styles/pages/Slots.module.css';
 
 export default function Slots() {
     return (
         <Page className={styles.root} title={'Core: Slots'}>
             <SpacerSection />
-            <Title>A:B:C</Title>
+            <Title>
+                <b>Landing</b> is built using Slots.
+            </Title>
             <SpacerSection />
-            <HelveticaNeue>
-                We are again applying ratios, the relation between any magnitudes with respect to the number of times one contains the others. It gives
-                overlaying elements without resorting to tricky positioning and less reliant on magic numbers.
-            </HelveticaNeue>
+            <Code children={AnatomyProps} />
             <SpacerParagraph />
-            <SlotsDemo />
+            <Title>
+                Works for card <b>content</b> too.{' '}
+            </Title>
             <SpacerSection />
-            <HelveticaNeue>Full control on both ratios to adjust at will.</HelveticaNeue>
+            <Card>
+                <SlotCard />
+            </Card>
             <SpacerParagraph />
-            <Stat />
-            <SpacerSection />
-            <Title>You could also build card content with it.</Title>
-            <SpacerSection />
-            <Grid size="520px" gap="1em">
-                <Card>
-                    <SlotCard />
-                </Card>
-            </Grid>
+            <Code children={DemoCode} />
             <SpacerSection />
         </Page>
     );
 }
+
+const AnatomyProps = `// Props
+interface Props extends Pick<DefaultProps, 'as' | 'id' | 'className'> {
+    /** Defaults to  0 max(5vw, 2rem);*/
+    gap?: string;
+    /** Array of SlotItemProps */
+    portrait: Array<SlotItemProps>;
+    /** Array of SlotItemProps */
+    landscape: Array<SlotItemProps>;
+    /** 🚨 Divisions across X axis */
+    x: number;
+    /** 🚨 Divisions across Y axis */
+    y: number;
+    /** Assign min height to each slot item */
+    minY?: string;
+    /** debug */
+    debug?: boolean;
+}
+
+// Data driven items
+type DictNumber = Record<number, number>;
+
+export interface SlotItemProps extends Partial<ActionItem> {
+    /** grid-template-columns [start,end]*/
+    x: DictNumber;
+    /** grid-template-rows [start,end]*/
+    y: DictNumber;
+    as?: 'div' | 'article' | 'aside';
+    /** grid content alignment , defaults to stretch */
+    placement?: 'end' | 'start' | 'stretch' | 'center';
+}
+`;
+
+const DemoCode = `// Demo Card 
+export default function SlotCard() {
+    const slotsPortrait: Array<SlotItemProps> = [
+        {
+            id: '0',
+            label: '',
+            children: <Artwork />,
+            x: [1, 7],
+            y: [1, 4],
+        },
+        {
+            id: '3',
+            label: '',
+            children: <Content />,
+            x: [1, 7],
+            y: [4, 6],
+        },
+        {
+            id: '4',
+            label: '',
+            children: <ButtonAccent className="stretch-width-recursive unset-height">Main Action</ButtonAccent>,
+            placement: 'end',
+            x: [1, 7],
+            y: [6, 7],
+        },
+    ];
+    const slotsLandscape: Array<SlotItemProps> = [
+        {
+            id: '1',
+            label: '',
+            children: <Artwork />,
+            x: [1, 4],
+            y: [1, 7],
+        },
+        {
+            id: '2',
+            label: '',
+            children: <Content />,
+            x: [4, 7],
+            y: [1, 6],
+        },
+        {
+            id: '4',
+            label: '',
+            children: <ButtonAccent className="stretch-width-recursive unset-height">Main Action</ButtonAccent>,
+            placement: 'end',
+            x: [4, 7],
+            y: [6, 7],
+        },
+    ];
+
+    return <Slots portrait={slotsPortrait} landscape={slotsLandscape} x={6} y={6} gap="1em" />;
+}`;
